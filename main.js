c7f6f48bfd37ff994f4db93f992001785f5208fa
JakeFritz23 <jake.fritzel@yahoo.com>
2025-04-28 14:18:23 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// Main glue file for FortressMail scripts
+
+// Ensure all components are properly loaded and available
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('FortressMail App Loaded Successfully.');
+});
